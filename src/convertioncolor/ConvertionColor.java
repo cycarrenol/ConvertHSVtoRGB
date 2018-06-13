5aff0ea82d73420dbf591c95955cd0707be260b1
gaapl <gaapl@DESKTOP-U64DP39>
2018-06-12 20:51:15 -0500

    PRIMER COMMIT

--- src/convertioncolor/ConvertionColor.java
+++ src/convertioncolor/ConvertionColor.java
@@ -4,65 +4,69 @@ public class ConvertionColor {
 
     public static void main(String[] args) {
 
-        int H =120;//hue
-        int S = 1;//saturation
-        int V = 1;//value
+        //CONVERTIR HSV A RGB
+        double H = 70;//hue
+        double S = 1;//saturation
+        double V = 1;//value
 
-        int R = 0;//Red
-        int G = 0;//Blue
-        int B = 0;//Green
-        
+        double R1 = 0;//Red
+        double G1 = 0;//Blue
+        double B1 = 0;//Green
+
         //Begin the formula
-        int C = V * S;
+        double C = V * S;
 
-        int X = C * (1 - Math.abs((H / 60) % 2 - 1));
-
-        int m = V - C;
+        double X = C * (1 - Math.abs(((H / 60) % 2) - 1));
+        System.out.println("°°" + X);
+        double m = V - C;
 
         if (H >= 0 && H < 60) {
 
-            R = (C + m) * 255;
-            G = (X + m) * 255;
-            B = (0 + m) * 255;
+            R1 = (C + m) * 255;
+            G1 = (X + m) * 255;
+            B1 = (0 + m) * 255;
 
         } else if (H >= 60 && H < 120) {
 
-            R = (X + m) * 255;
-            G = (C + m) * 255;
-            B = (0 + m) * 255;
+            R1 = (X + m) * 255;
+            G1 = (C + m) * 255;
+            B1 = (0 + m) * 255;
 
         } else if (H >= 120 && H < 180) {
 
-            R = (0 + m) * 255;
-            G = (C + m) * 255;
-            B = (X + m) * 255;
+            R1 = (0 + m) * 255;
+            G1 = (C + m) * 255;
+            B1 = (X + m) * 255;
 
         } else if (H >= 180 && H < 240) {
 
-            R = (0 + m) * 255;
-            G = (X + m) * 255;
-            B = (C + m) * 255;
+            R1 = (0 + m) * 255;
+            G1 = (X + m) * 255;
+            B1 = (C + m) * 255;
 
         } else if (H >= 240 && H < 300) {
 
-            R = (X + m) * 255;
-            G = (0 + m) * 255;
-            B = (C + m) * 255;
+            R1 = (X + m) * 255;
+            G1 = (0 + m) * 255;
+            B1 = (C + m) * 255;
 
         } else if (H >= 300 && H < 360) {
 
-            R = (C + m) * 255;
-            G = (0 + m) * 255;
-            B = (X + m) * 255;
+            R1 = (C + m) * 255;
+            G1 = (0 + m) * 255;
+            B1 = (X + m) * 255;
 
         }
-         //End the formula
-         
-         //print the RGB
-        System.out.println(R);
-        System.out.println(G);
-        System.out.println(B);
 
+        int R2 = (int) R1;
+        int G2 = (int) G1;
+        int B2 = (int) B1;
+
+        System.out.println(R2);
+        System.out.println(G2);
+        System.out.println(B2);
+
+        // FIN CONVERSION
     }
 
 }
